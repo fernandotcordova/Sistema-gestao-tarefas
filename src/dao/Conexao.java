9c78c70c473ea7112ac4975fc1a17752377d09f4
Fernando Cordova <fernandoturela3@gmail.com>
2026-09-13 17:24:45 -0300

    Update database credentials in Conexao.java

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class Conexao {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/gerenciadorTarefas";
-    private static final String USUARIO = "postgresql";
-    private static final String SENHA = "password";
+    private static final String USUARIO = "seuUsuarioAqui";
+    private static final String SENHA = "suaSenhaAqui";
 
     // Método de conexão
     public static Connection getConnection() throws SQLException, SQLException {
